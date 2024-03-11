2e5749c87f2038203ee384d24323c4925ec269e6
shubham kanaujiya <shubhamkanaujiya505@gmail.com>
2024-03-11 17:04:55 +0530

    Loading multiple views

---
+++ app/Views/myview.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COdeignitor</title>
+</head>
+<body>
+    <h4>welcome to Codeignitor 4 </h4>
+</body>
+</html>
