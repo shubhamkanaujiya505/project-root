ec8442f538d35d34025a8c451e2dee54d90b1fb6
shubham kanaujiya <shubhamkanaujiya505@gmail.com>
2024-03-12 12:52:47 +0530

    section layout view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,27 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'pageHeading' => 'This is home page'
+        ];
+        echo view("layout/header",$data);
+        echo view("homeview" );
+        echo view("layout/footer");
+    }
+    public function about()
+    {
+        $data = [
+            'pageHeading' => 'This is about page'
+        ];
+        echo view("layout/header",$data);
+        echo view("aboutview");
+        echo view("layout/footer");
     }
 }
